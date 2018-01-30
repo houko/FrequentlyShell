b3b889323f60f9d83c758e3c07d5db4e406c7744
xiaomo <xiaomo@xiaomo.info>
2018-01-30 17:06:07 +0800

    streak

--- python/streak.py
+++ python/streak.py
@@ -17,32 +17,35 @@ import datetime
 import os
 
 
+# 修改文件
 def modify():
-    file = open('test', 'r')
-    flag = int(file.readline()) == 0
-    file.close()
-    file = open('test', 'w+')
-    if flag:
-        file.write('1')
-    else:
-        file.write('0')
-        file.close()
+    with open('test', 'r') as read:
+        flag = int(read.readline()) == 0
+        with open('test', 'w+') as write:
+            if flag:
+                write.write('1')
+            else:
+                write.write('0')
 
 
+# 提交代码
 def commit():
     os.system('git commit -a -m test_github_streak > /dev/null 2>&1')
 
 
+# 设置系统时间
 def set_sys_time(year, month, day):
     os.system('date -s %04d%02d%02d' % (year, month, day))
 
 
+# 提交代码
 def trick_commit(year, month, day):
     set_sys_time(year, month, day)
     modify()
     commit()
 
 
+# 日常提交
 def daily_commit(start_date, end_date):
     for i in range((end_date - start_date).days + 1):
         cur_date = start_date + datetime.timedelta(days=i)
